ddc74b6e0fa0801dfc72824423b886070a170aa6
Mauricio Vargas <65517229+MauriVargas17@users.noreply.github.com>
2022-06-28 12:52:59 -0400

    First way to create token

--- PetoCoin.sol
+++ PetoCoin.sol
@@ -5,6 +5,8 @@ pragma solidity ^0.8.0;
 // Requires 6 functions at least
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+
+// By using this model, we can only transfer tokens, but cannot sell or destroy tokens
 contract PetoCoin is ERC20 {
     constructor(uint256 initialSupply) ERC20("Peto Coin", "PETO") {
         _mint(msg.sender, initialSupply);
