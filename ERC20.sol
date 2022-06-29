7a0370c9abd0f024637e1b3ae6714a40edaaa406
Mauricio Vargas <65517229+MauriVargas17@users.noreply.github.com>
2022-06-29 12:48:58 -0400

    Internal functions in ERC20

--- ERC20.sol
+++ ERC20.sol
@@ -41,7 +41,7 @@ contract ERC20 is IERC20, IERC20Metadata {
 
     function transfer(address _to, uint256 _value) public override returns (bool success){
         address owner = msg.sender;
-        _transfer(owner, to, amount);
+        _transfer(owner, _to, _value);
         return true;
     }
 
@@ -61,5 +61,43 @@ contract ERC20 is IERC20, IERC20Metadata {
         _transfer(_from, _to, _value);
         return true;
     }
-    
+
+    function _transfer(address from, address to, uint256 amount) internal {
+        require(from != address(0), "ERC20: Do not transfer from the zero address.");
+        require(from != address(0), "ERC20: Do not transfer to the zero address.");
+
+        uint256 fromBalance = _balances[from];
+        require(fromBalance >= amount, "ERC20: Transfer amount exceeds balance.");
+
+    // Save in gas fees
+        unchecked {
+            _balances[from] = fromBalance - amount;
+        }
+
+        _balances[to] += amount;
+
+        emit Transfer(from, to, amount);
+
+    }
+
+    function _approve(address owner, address spender, uint256 amount) internal {
+        require(owner != address(0), "ERC20: Do not approve from the zero address.");
+        require(spender != address(0), "ERC20: Do not approve to the zero address.");
+
+        _allowances[owner][spender] = amount;
+        emit Approval(owner, spender, amount);
+
+    }
+
+    function _spendAllowance(address owner, address spender, uint256 amount)internal {
+        uint256 currentAllowance = allowance(owner, spender);
+        if (currentAllowance < type(uint256).max){
+            require(currentAllowance >= amount, "ERC20: Current allowance is not enough to spend the intended amount");
+            unchecked{
+                _approve(owner, spender, currentAllowance - amount);
+            }
+        }
+    }
+
 }
+
